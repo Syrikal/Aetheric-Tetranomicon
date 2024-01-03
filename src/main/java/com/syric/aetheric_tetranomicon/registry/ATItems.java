de42afc918db9d407f6adc7f50bc2d6d65e4d78c
Syric <76715986+Syrikal@users.noreply.github.com>
2024-01-03 00:32:50 -0500

    Added Valkyrian scrap model and recipes, Valkyrian binding module, and improvement names.

--- src/main/java/com/syric/aetheric_tetranomicon/registry/ATItems.java
+++ src/main/java/com/syric/aetheric_tetranomicon/registry/ATItems.java
@@ -1,7 +1,15 @@
 package com.syric.aetheric_tetranomicon.registry;
 
+import com.aetherteam.aether.item.AetherCreativeTabs;
+import com.aetherteam.aether.item.AetherItems;
 import com.syric.aetheric_tetranomicon.AethericTetranomicon;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -11,6 +19,15 @@ public class ATItems {
     public static final RegistryObject<Item> VALKYRIAN_SCRAP = ITEMS.register("valkyrian_scrap", () -> new Item(new Item.Properties()));
 
     public ATItems() {
+    }
+
+    @SubscribeEvent
+    public static void buildCreativeModeTabs(BuildCreativeModeTabContentsEvent event) {
+        ResourceKey<CreativeModeTab> tab = event.getTabKey();
+        if (tab == AetherCreativeTabs.AETHER_INGREDIENTS.getKey()) {
+            event.getEntries().putAfter(new ItemStack(AetherItems.SWET_BALL.get()), new ItemStack((ItemLike) VALKYRIAN_SCRAP.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        }
+
     }
 
 }
