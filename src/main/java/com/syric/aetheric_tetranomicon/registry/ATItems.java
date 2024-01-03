56a947e1281d78de1a9962c817d2862751c9d785
Syric <76715986+Syrikal@users.noreply.github.com>
2024-01-03 11:42:15 -0500

    Added Valkyrian Scrap to Creative menu

--- src/main/java/com/syric/aetheric_tetranomicon/registry/ATItems.java
+++ src/main/java/com/syric/aetheric_tetranomicon/registry/ATItems.java
@@ -3,17 +3,20 @@ package com.syric.aetheric_tetranomicon.registry;
 import com.aetherteam.aether.item.AetherCreativeTabs;
 import com.aetherteam.aether.item.AetherItems;
 import com.syric.aetheric_tetranomicon.AethericTetranomicon;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+@Mod.EventBusSubscriber(
+        modid = "aetheric_tetranomicon",
+        bus = Mod.EventBusSubscriber.Bus.MOD
+)
 public class ATItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AethericTetranomicon.MODID);
     public static final RegistryObject<Item> VALKYRIAN_SCRAP = ITEMS.register("valkyrian_scrap", () -> new Item(new Item.Properties()));
@@ -23,9 +26,9 @@ public class ATItems {
 
     @SubscribeEvent
     public static void buildCreativeModeTabs(BuildCreativeModeTabContentsEvent event) {
-        ResourceKey<CreativeModeTab> tab = event.getTabKey();
-        if (tab == AetherCreativeTabs.AETHER_INGREDIENTS.getKey()) {
-            event.getEntries().putAfter(new ItemStack(AetherItems.SWET_BALL.get()), new ItemStack((ItemLike) VALKYRIAN_SCRAP.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        CreativeModeTab tab = event.getTab();
+        if (tab == AetherCreativeTabs.AETHER_INGREDIENTS.get()) {
+            event.getEntries().putAfter(new ItemStack(AetherItems.SWET_BALL.get()), new ItemStack(VALKYRIAN_SCRAP.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
         }
 
     }
